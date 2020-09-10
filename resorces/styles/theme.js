887c48a91b71c045fbda47def8bea0bb2bde6e89
Shane Huang <shane.huang@sinopac.com>
2020-09-10 11:27:32 +0800

    feat(style): create a site theme

---
+++ resorces/styles/theme.js
@@ -0,0 +1,14 @@
+const theme = {
+    fontFamily: 'Arial, "儷黑 Pro", "LiHei Pro", "微軟正黑體", "Microsoft JhengHei", sans-serif',
+    colors: {
+        text: '#ffffff',
+        darkBg: '#0d1623',
+        primary: '#c43826',
+        primaryHover: '#9d1200',
+        secondary: '#daa360',
+        secondaryHover: '#ad7633',
+        loss: '#22a16f',
+    },
+};
+
+export default theme;
