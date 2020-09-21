8b8f395e0b4c1f3581640b2e4650c55415d13ce0
Shane Huang <shane.huang@sinopac.com>
2020-09-21 13:55:35 +0800

    fix(myAxios): define server or client environment

--- services/myAxios.js
+++ services/myAxios.js
@@ -4,13 +4,16 @@ import { Modal } from 'antd';
 axios.interceptors.response.use(res => {
     return res;
 }, error=>{
+    const isServer = typeof window === 'undefined';
+    const defaultErrorMsg = '伺服器錯誤，請稍後再試';
+
     if(error.response == null){
-        Modal.error({content: '伺服器錯誤，請稍後再試'});
+        !isServer && Modal.error({content: defaultErrorMsg});
     }else{
         if(error.response.data != null){
-            Modal.error({content: error.response.data.message || '伺服器錯誤，請稍後再試'});
+            !isServer && Modal.error({content: error.response.data.message || defaultErrorMsg});
         }else{
-            Modal.error({content: '伺服器錯誤，請稍後再試'});
+            !isServer && Modal.error({content: defaultErrorMsg});
         }
     }
     return Promise.reject(error)
