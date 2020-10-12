1806432c7ec0ccd3781008745236097174565328
Shane Huang <shane.huang@sinopac.com>
2020-10-12 17:16:02 +0800

    feat(LastUpdatedTime): create component

--- components/includes/personalArea/LastUpdatedTime.js
+++ components/includes/personalArea/LastUpdatedTime.js
@@ -1,7 +1,42 @@
-export const LastUpdatedTime = () => {
+import PropTypes from 'prop-types';
+
+import theme from '../../../resources/styles/theme';
+import reloadImg from '../../../resources/images/components/header/ic_reload.png';
+
+export const LastUpdatedTime = ({ time }) => {
     return (
         <div className="lastUpdatedTime__container">
-            <style jsx>{``}</style>
+            <span>
+                {'更新時間'} {time}
+            </span>
+            <div className="button__reload">
+                <img src={reloadImg} alt="reload"></img>
+            </div>
+            <style jsx>{`
+                .lastUpdatedTime__container {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    padding: 8px 0;
+                    font-size: 1.2rem;
+                    color: #0d1623;
+                }
+                .button__reload {
+                    cursor: pointer;
+                }
+                @media (max-width: ${theme.mobileBreakPoint}px) {
+                    .lastUpdatedTime__container {
+                        padding: 8px 20px;
+                        font-size: 1.4rem;
+                        color: #a9b6cb;
+                        border-bottom: solid 1px #17273d;
+                    }
+                }
+            `}</style>
         </div>
     );
+};
+
+LastUpdatedTime.propTypes = {
+    time: PropTypes.string.isRequired,
 };
