c339a0aee0adea1b4bcebe11f2c8b4c3d18d5b72
鄭乃倫-系統開發一部-永豐金證券 <bryan.cheng@sinopac.com>
2020-10-15 16:35:18 +0800

    左選單修正

--- components/includes/navbar/navbar.js
+++ components/includes/navbar/navbar.js
@@ -5,20 +5,22 @@ import Link from 'next/link'
 import theme from '../../../resources/styles/theme'
 import NavList from "../navbar/navList"
 import logo from '../../../resources/images/components/header/sinopac_securities_logo.png';
-import closemenu from '../../../resources/images/components/header/ic_closemenu.png';
+import closeMenu from '../../../resources/images/components/header/ic_closemenu.png';
+import closeImg from '../../../resources/images/components/header/ic_close_horizontal_flip.png';
+import openImg from '../../../resources/images/components/header/ic_open.png';
 
-
+// firefox 手機板隱藏 navbar scrollBar
+// level(1) 選單點選第二次隱藏
 
 
 const Navbar = React.memo((props) => {
 
     const serverMainlNav = useSelector((store) => store.server.navData?.main);
     const clientMainlNav = useSelector((store) => store.layout.navData?.main);
+    const isMobile = useSelector((store) => store.layout.isMobile);
     const showMenu = useSelector(store => store.layout.showMenu);
 
     const mainNav = clientMainlNav ? clientMainlNav : serverMainlNav;
-    const [lv2MobileVisible, setLv2MobileVisible] = useState(false);
-
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -27,20 +29,30 @@ const Navbar = React.memo((props) => {
 
     const resizeHandler = function(){
         let winWidth = window.innerWidth;
-        
         if (winWidth <= 1024){
             dispatch(setMenuOpen(false));
-        }
+        } 
     }
 
     const menuClickHandler = function(){
         dispatch(setMenuOpen(false));
     }
 
+    const menuItemClickHandler = (dom) => {
+        if (dom.target.parentElement.getElementsByClassName("navbar__lv2")[0].classList.contains("navbar__lv2--show")) {
+            dom.target.parentElement.getElementsByClassName("navbar__lv2")[0].classList.remove("navbar__lv2--show")
+            dom.target.parentElement.classList.remove("navbar__lv1__item--show")
+        } else {
+            dom.target.parentElement.getElementsByClassName("navbar__lv2")[0].classList.add("navbar__lv2--show")
+            dom.target.parentElement.classList.add("navbar__lv1__item--show")
+        }
+        
+    }
+
     return (
         <ul className={`navbar ${showMenu ? '' : 'navbar--hide'}`} >
             
-            <li className="navbar__lv1__item mobile__menu__ctrl"> 
+            <li className="navbar__lv1__item mobile__menu__ctrl" > 
                 <span className="navbar__lv1__item__title menu__ctrl">
                     <Link href="/">
                         <a className="header__logo">
@@ -49,7 +61,7 @@ const Navbar = React.memo((props) => {
                     </Link>
                     <Link href="/">
                         <a className="close__menu" onClick={menuClickHandler}>
-                            <img src={closemenu}></img>
+                            <img src={closeMenu}></img>
                         </a>
                     </Link>
                 </span> 
@@ -58,12 +70,14 @@ const Navbar = React.memo((props) => {
                 
                 <li className="navbar__lv1__item" key={lv1Index}>
                     <Link href="/">
-                        <a className="navbar__lv1__item__title">
+                        <a className="navbar__lv1__item__title" onClick={menuItemClickHandler}>
                             <span className="active__mark"></span>
-                            {lv1Item.title}
+                            {lv1Item.title} 
+
                         </a>
                     </Link>
-                    <ul className={`navbar__lv2 ${lv2MobileVisible ? "navbar__lv2--show" : ""} ${lv1Index > (mainNav.length/2) ? "right" : "" }` } style={{ width: 168 * lv1Item.items.length }}>
+                    
+                    <ul className={`navbar__lv2 ${lv1Index > (mainNav.length/2) ? "right" : "" }` } style={{ width: 168 * lv1Item.items.length }}>
                         {lv1Item.items.map((lv2Item, lv2Index) => (
                             <li className="navbar__lv2__item" key={lv2Index} >
                                 <NavList navItems={lv1Item.items} lv2Data={lv2Item} twoColumnPX={1024}/>
@@ -130,16 +144,6 @@ const Navbar = React.memo((props) => {
                     display:none;
                 }
 
-                .navbar__lv1__item__title:hover ,.navbar__lv1__item.active{
-                    color: ${theme.colors.secondary};
-                }
-                .navbar__lv1__item__title:hover > .active__mark, .navbar__lv1__item.active .active__mark{
-                    display:block;
-                }
-                .navbar__lv1__item:hover .navbar__lv2 { 
-                    display:flex;
-                }
-
                 .navbar__lv2 {
                     margin:0;
                     padding:0;
@@ -169,8 +173,30 @@ const Navbar = React.memo((props) => {
                     margin-right: 0;
                 }
 
+                @media (min-width:1024px) {
+                    .navbar__lv1__item:hover .navbar__lv1__item__title ,.navbar__lv1__item.active{
+                        color: ${theme.colors.secondary};
+                    }
+                    .navbar__lv1__item:hover .active__mark, .navbar__lv1__item.active .active__mark{
+                        display:block;
+                    }
+                    .navbar__lv1__item:hover .navbar__lv2 { 
+                        display:flex;
+                    }
+                    .navbar__lv1__item:hover .navbar__lv1__item__title:after { 
+                        transition: all .3s;
+                        transform: rotate(-0deg);
+                    }
+                }
+
                 @media (max-width:1024px) {
+
+                    .navbar__lv2--show {
+                        display:flex;
+                    }
+
                     .navbar {
+                        -ms-overflow-style: none;
                         background : ${theme.colors.darkBg};
                         flex-direction: column;
                         position: fixed;
@@ -182,6 +208,10 @@ const Navbar = React.memo((props) => {
                         height: auto;
                         min-height: calc(100% - 140px);
                         max-height: calc(100% - 140px);
+                        border-right: solid ${theme.colors.darkBg} 1px;
+                    }
+                    .navbar::-webkit-scrollbar {
+                        display: none;
                     }
                     .close__menu {
                         float:right;
@@ -192,7 +222,6 @@ const Navbar = React.memo((props) => {
                     .navbar__lv1__item.mobile__menu__ctrl {
                         display: block;
                     }
-
                     .navbar__lv1__item.navbar__shortcuts__li {
                         display: block;
                         height: 70px;
@@ -235,11 +264,39 @@ const Navbar = React.memo((props) => {
                     }
                     .navbar__lv1__item .navbar__lv2--show { 
                         display:flex;
+                        transition: all .3s;
                     }
                     .navbar__lv1__item__title {
                         height: auto;
                         padding: 0 30px;
+                        color:#FFF;
                     }
+                    
+                    .navbar__lv1__item__title:after {
+                        content: '';
+                        width: 32px;
+                        height: 32px;
+                        background-image: url(${openImg});
+                        margin-left: 4px;
+                        transition: all .3s;
+                        transform: rotate(-180deg);
+                        display: inline-block;
+                        vertical-align: middle;
+                    }
+
+                    .navbar__lv1__item__title.menu__ctrl:after {
+                        display:none;
+                    }
+
+                    .navbar__lv1__item--show .navbar__lv1__item__title:after {
+                        background-image: url(${closeImg});
+                        transform: rotate(0);
+                    }
+
+                    .navbar__lv1__item--show .navbar__lv1__item__title {
+                        color: ${theme.colors.secondary};
+                    }
+
                     .navbar__lv2 {
                         position: relative;
                         top: 0;
