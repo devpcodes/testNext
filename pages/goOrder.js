e565752ed6385a1191e0f16afa3376ce39467369
wilson <sc.wilsonlin@gmail.com>
2020-10-22 11:02:03 +0800

    fix(goOrder, tradingAccount): 修正

--- pages/goOrder.js
+++ pages/goOrder.js
@@ -1,12 +1,30 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { wrapper } from '../store/store';
 import { setNavItems } from '../store/components/layouts/action';
-
+import { objectToQueryHandler } from '../services/objectToQueryHandler';
+import { useRouter } from 'next/router';
 export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
     await store.dispatch(setNavItems());
 });
 function GoOrder() {
+    const router = useRouter();
+    const [queryStr, setQueryStr] = useState('');
+
+    useEffect(() => {
+        const qStr = objectToQueryHandler(router.query);
+        if (qStr) {
+            setQueryStr(qStr);
+        }
+    }, []);
+
+    useEffect(() => {
+        const qStr = objectToQueryHandler(router.query);
+        if (qStr) {
+            setQueryStr(qStr);
+        }
+    }, [router.query]);
     return (
         <>
             <Head>
@@ -14,7 +32,11 @@ function GoOrder() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div>
-                <NewWebIframe iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/goOrder`} title="永豐金證券" />
+                <NewWebIframe
+                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/goOrder${queryStr}`}
+                    title="永豐金證券"
+                    iHeight={830}
+                />
             </div>
         </>
     );
