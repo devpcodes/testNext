5cb5a613383cb5640d7f39fa5ce9743df06221fb
wilson <sc.wilsonlin@gmail.com>
2020-10-25 17:40:10 +0800

    fix(navigation): location 修正

--- pages/navigation.js
+++ pages/navigation.js
@@ -1,62 +1,62 @@
 import { submit } from '../services/components/login/trustLogin';
 
 const OpenAccount = () => {
-    
-
-    const getQueryString = (name) => {
-        const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-        const r = location.search.substr(1).match(reg);
-        if (r != null) return unescape(decodeURI(r[2]));
+    const getQueryString = name => {
+        const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+        const isServer = typeof window === 'undefined';
+        if (!isServer) {
+            const r = location.search.substr(1).match(reg);
+            if (r != null) return unescape(decodeURI(r[2]));
+        }
         return null;
-    }
+    };
 
     const queryStringDict = {
-        platform: getQueryString("platform"),
-        source: getQueryString("source"),
-        page: getQueryString("page") ? getQueryString("page") : "/",
-        otp: getQueryString("otp")
-    }
+        platform: getQueryString('platform'),
+        source: getQueryString('source'),
+        page: getQueryString('page') ? getQueryString('page') : '/',
+        otp: getQueryString('otp'),
+    };
 
     const canTrustDict = {
-        'Bond_Securities' : {
-            target: "/",
+        Bond_Securities: {
+            target: '/',
             needLogin: true,
-            errorPage: "/",
-            loginType: 'trust'
+            errorPage: '/',
+            loginType: 'trust',
         },
-        'MMA': {
-            target: "/",
+        MMA: {
+            target: '/',
             needLogin: true,
-            errorPage: "/",
-            loginType: 'trust'
+            errorPage: '/',
+            loginType: 'trust',
         },
-        'Line': {
-            target: "/",
+        Line: {
+            target: '/',
             needLogin: false,
-            errorPage: "/",
-            loginType: 'trust'
-        }
-    }
+            errorPage: '/',
+            loginType: 'trust',
+        },
+    };
 
     const doLogin = (async () => {
         try {
             if (canTrustDict[queryStringDict.platform].needLogin) {
                 const res = await submit(queryStringDict.otp);
-                console.log(res)
+                console.log(res);
                 if (res.data.success === true) {
                     sessionStorage.setItem('source', queryStringDict.platform.toLowerCase());
                     // 導回設定頁面
                 }
             }
         } catch (e) {
-            console.log(e)
-            alert("失敗")
+            console.log(e);
+            alert('失敗');
             // 導回未登入首頁 ??
         }
     })();
-    
-    
-    return ""; 
+
+    return '';
 };
 
 export default OpenAccount;
