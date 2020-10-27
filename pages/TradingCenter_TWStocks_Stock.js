0242d0d126f535b63ccff445abd41372becefd11
Shane Huang <shane.huang@sinopac.com>
2020-10-27 14:36:25 +0800

    feat(TradingCenter_TWStocks_Stock): add ability to get querystring

--- pages/TradingCenter_TWStocks_Stock.js
+++ pages/TradingCenter_TWStocks_Stock.js
@@ -1,22 +1,36 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { wrapper } from '../store/store';
 import { setNavItems } from '../store/components/layouts/action';
 import { useSelector } from 'react-redux';
 import { PageHead } from '../components/includes/PageHead';
+import { objectToQueryHandler } from '../services/objectToQueryHandler';
 
 export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
     await store.dispatch(setNavItems());
 });
 
 function TradingCenter_TWStocks_Stock() {
+    const router = useRouter();
+    const [queryStr, setQueryStr] = useState('');
     const isLogin = useSelector(store => store.user.isLogin);
     const isMobile = useSelector(store => store.layout.isMobile);
+
+    useEffect(() => {
+        const qStr = objectToQueryHandler(router.query);
+        if (qStr) {
+            setQueryStr(qStr);
+        }
+    }, [router.query]);
+
     return (
         <>
             <PageHead title={'個股報價'} />
             <div>
                 <NewWebIframe
-                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/TradingCenter_TWStocks_Stock`}
+                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/TradingCenter_TWStocks_Stock${queryStr}`}
                     iHeight={isMobile ? 1900 : 1450}
                     title="永豐金證券"
                     login={isLogin}
