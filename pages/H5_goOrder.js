6a7141d8b071a32252cda81fd2974b0a90da52f7
wilson <sc.wilsonlin@gmail.com>
2020-10-29 11:50:25 +0800

    H5修正

--- pages/H5_goOrder.js
+++ pages/H5_goOrder.js
@@ -1,13 +1,33 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { PageHead } from '../components/includes/PageHead';
+import { objectToQueryHandler } from '../services/objectToQueryHandler';
 
 function H5_goOrder() {
+    const router = useRouter();
+    const [queryStr, setQueryStr] = useState('');
+
+    useEffect(() => {
+        const qStr = objectToQueryHandler(router.query);
+        if (qStr) {
+            setQueryStr(qStr);
+        }
+    }, []);
+
+    useEffect(() => {
+        const qStr = objectToQueryHandler(router.query);
+        if (qStr) {
+            setQueryStr(qStr);
+        }
+    }, [router.query]);
+
     return (
         <>
             <PageHead title={'即時盯盤'} />
             <div>
                 <NewWebIframe
-                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/H5_goOrder`}
+                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/H5_goOrder${queryStr}`}
                     title="永豐金證券"
                     iHeight={88}
                 />
