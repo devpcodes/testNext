b2061c4b138eb51030f6b314d0277cf430fedd98
wilson <sc.wilsonlin@gmail.com>
2020-10-29 09:55:36 +0800

    H5 整合測試

---
+++ pages/H5_Test.js
@@ -0,0 +1,25 @@
+import NewWebIframe from '../components/includes/NewWebIframe';
+import { PageHead } from '../components/includes/PageHead';
+
+function H5_Test() {
+    return (
+        <>
+            <PageHead title={'即時盯盤'} />
+            <div>
+                <NewWebIframe
+                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/H5_Test`}
+                    title="永豐金證券"
+                    iHeight={1200}
+                />
+            </div>
+        </>
+    );
+}
+
+H5_Test.getLayout = page => (
+    <>
+        <H5_Test>{page}</H5_Test>
+    </>
+);
+
+export default H5_Test;
---
+++ pages/H5_TradingAccount.js
@@ -0,0 +1,25 @@
+import NewWebIframe from '../components/includes/NewWebIframe';
+import { PageHead } from '../components/includes/PageHead';
+
+function H5_TradingAccount() {
+    return (
+        <>
+            <PageHead title={'交易帳務'} />
+            <div>
+                <NewWebIframe
+                    iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/H5_TradingAccount`}
+                    title="永豐金證券"
+                    iHeight={410}
+                />
+            </div>
+        </>
+    );
+}
+
+H5_TradingAccount.getLayout = page => (
+    <>
+        <H5_TradingAccount>{page}</H5_TradingAccount>
+    </>
+);
+
+export default H5_TradingAccount;
