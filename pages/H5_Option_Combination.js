bf8da3a4b9578a00e604b7a65cfb4d96ea51154e
Shane Huang <shane.huang@sinopac.com>
2020-10-30 16:59:30 +0800

    fix(H5_Option_Combination): open window

--- pages/H5_Option_Combination.js
+++ pages/H5_Option_Combination.js
@@ -1,7 +1,36 @@
+import { useEffect, useRef } from 'react';
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { PageHead } from '../components/includes/PageHead';
 
 function H5_Option_Combination() {
+    const iframeDom = useRef(null);
+    const source = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener('message', messageEventHandler);
+
+        return () => {
+            window.removeEventListener('message', messageEventHandler);
+        };
+    }, []);
+
+    const messageEventHandler = e => {
+        if (e.data.optionList != null) {
+            iframeDom.current.contentWindow.postMessage(e.data, '*');
+            source.current = e.source;
+        }
+        if (e.data.optionList == null) {
+            if (e.data.product != null) {
+                source.current.postMessage(e.data, '*');
+                window.close();
+            }
+        }
+    };
+
+    const getIframeDom = dom => {
+        iframeDom.current = dom;
+    };
+
     return (
         <>
             <PageHead title={'即時盯盤'} />
@@ -9,7 +38,8 @@ function H5_Option_Combination() {
                 <NewWebIframe
                     iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/H5_Option_Combination`}
                     title="永豐金證券"
-                    iHeight={903}
+                    iHeight={550}
+                    getIframeDom={getIframeDom}
                 />
             </div>
         </>
