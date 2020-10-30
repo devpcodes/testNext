71e9e8aeb30cdde7abb9d7205624651a2eb94531
Shane Huang <shane.huang@sinopac.com>
2020-10-30 17:08:26 +0800

    fix(H5_Future): open window

--- pages/PlaceOrderPopupWindow.js
+++ pages/PlaceOrderPopupWindow.js
@@ -1,7 +1,36 @@
+import { useEffect, useRef } from 'react';
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { PageHead } from '../components/includes/PageHead';
 
 function PlaceOrderPopupWindow() {
+    const iframeDom = useRef(null);
+    const source = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener('message', messageEventHandler);
+
+        return () => {
+            window.removeEventListener('message', messageEventHandler);
+        };
+    }, []);
+
+    const messageEventHandler = e => {
+        if (e.data.optionList != null) {
+            iframeDom.current.contentWindow.postMessage(e.data, '*');
+            source.current = e.source;
+        }
+        if (e.data.optionList == null) {
+            if (e.data.product != null) {
+                source.current.postMessage(e.data, '*');
+                window.close();
+            }
+        }
+    };
+
+    const getIframeDom = dom => {
+        iframeDom.current = dom;
+    };
+
     return (
         <>
             <PageHead title={'即時盯盤'} />
@@ -10,6 +39,7 @@ function PlaceOrderPopupWindow() {
                     iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/PlaceOrderPopupWindow`}
                     title="永豐金證券"
                     iHeight={446}
+                    getIframeDom={getIframeDom}
                 />
             </div>
         </>
