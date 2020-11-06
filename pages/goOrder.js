b2c680ff318367b52bf4b0a6b16a956157f06966
Shane Huang <shane.huang@sinopac.com>
2020-11-06 09:53:48 +0800

    fix(goOrder): correct container margin

--- pages/goOrder.js
+++ pages/goOrder.js
@@ -1,20 +1,15 @@
 import { useState, useEffect } from 'react';
 import NewWebIframe from '../components/includes/NewWebIframe';
-import { wrapper } from '../store/store';
-import { setNavItems } from '../store/components/layouts/action';
 import { objectToQueryHandler } from '../services/objectToQueryHandler';
 import { useRouter } from 'next/router';
 import { PageHead } from '../components/includes/PageHead';
 import { checkServer } from '../services/checkServer';
 import { getParamFromQueryString } from '../services/getParamFromQueryString';
 
-export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
-    await store.dispatch(setNavItems());
-});
-
 function GoOrder() {
     const router = useRouter();
     const [queryStr, setQueryStr] = useState('');
+    const nav = router.query?.nav;
 
     useEffect(() => {
         const qStr = objectToQueryHandler(router.query);
@@ -26,13 +21,18 @@ function GoOrder() {
     return (
         <>
             <PageHead title={'快速下單'} />
-            <div>
+            <div className={'container'}>
                 <NewWebIframe
                     iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/goOrder${queryStr}`}
                     title="永豐金證券"
-                    iHeight={830}
+                    iHeight={710}
                 />
             </div>
+            <style jsx>{`
+                .container {
+                    margin-top: ${nav == '0' ? '0' : '70px'};
+                }
+            `}</style>
         </>
     );
 }
@@ -40,11 +40,12 @@ function GoOrder() {
 // nav=0 為無 header，nav=1 為有 header。預設為：有 header。
 const setGoOrderLayout = () => {
     const isServer = checkServer();
-    if (!isServer) {
+    if (isServer) {
+        GoOrder.getLayout = Page => Page;
+    } else {
         const nav = getParamFromQueryString('nav');
         if (nav == '0') {
-            // eslint-disable-next-line react/display-name
-            GoOrder.getLayout = page => <GoOrder>{page}</GoOrder>;
+            GoOrder.getLayout = Page => Page;
         }
     }
 };
