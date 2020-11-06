1fbe5725c8a5130658fc1d77de7b31f2b2d1b8b7
鄭乃倫-系統開發一部-永豐金證券 <bryan.cheng@sinopac.com>
2020-11-06 09:19:27 +0800

    Navigation 網址補上

--- pages/Navigation.js
+++ pages/Navigation.js
@@ -19,29 +19,27 @@ const Navigation = () => {
     const queryStringDict = {
         platform: getQueryString('platform'),
         source: getQueryString('source'),
-        page: getQueryString('page')
-            ? `${process.env.NEXT_PUBLIC_SUBPATH}${getQueryString('page')}`
-            : `${process.env.NEXT_PUBLIC_SUBPATH}`,
+        page: getQueryString('page') ? getQueryString('page') : ``,
         otp: getQueryString('otp'),
     };
 
     const canTrustDict = {
         Bond_Securities: {
-            target: `${process.env.NEXT_PUBLIC_SUBPATH}`,
+            target: ``,
             needLogin: true,
-            errorPage: `${process.env.NEXT_PUBLIC_SUBPATH}`,
+            errorPage: ``,
             loginType: 'trust',
         },
         MMA: {
-            target: `${process.env.NEXT_PUBLIC_SUBPATH}`,
+            target: ``,
             needLogin: true,
-            errorPage: `${process.env.NEXT_PUBLIC_SUBPATH}`,
+            errorPage: ``,
             loginType: 'trust',
         },
         Line: {
-            target: `${process.env.NEXT_PUBLIC_SUBPATH}`,
+            target: ``,
             needLogin: false,
-            errorPage: `${process.env.NEXT_PUBLIC_SUBPATH}`,
+            errorPage: ``,
             loginType: 'trust',
         },
     };
@@ -52,11 +50,11 @@ const Navigation = () => {
                 const res = await submit(queryStringDict.otp);
                 if (res.data.success === true) {
                     sessionStorage.setItem('source', queryStringDict.platform.toLowerCase());
-                    location.href = queryStringDict.page;
+                    location.href = `${process.env.NEXT_PUBLIC_SUBPATH}${queryStringDict.page}`;
                 }
             }
         } catch (e) {
-            location.href = canTrustDict[queryStringDict.platform].errorPage;
+            location.href = `${process.env.NEXT_PUBLIC_SUBPATH}${canTrustDict[queryStringDict.platform].errorPage}`;
             alert('登入失敗，請重新登入');
         }
     };
