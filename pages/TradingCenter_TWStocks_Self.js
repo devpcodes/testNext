5ebfbcf917617c3d6197e10c6716e29ce2fb1641
wilson <sc.wilsonlin@gmail.com>
2020-11-12 13:56:46 +0800

    fix(TradingCenter_twstocks_Self): 自選padding修正

--- pages/TradingCenter_TWStocks_Self.js
+++ pages/TradingCenter_TWStocks_Self.js
@@ -2,16 +2,17 @@ import { wrapper } from '../store/store';
 import { setNavItems } from '../store/components/layouts/action';
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { PageHead } from '../components/includes/PageHead';
-
+import { useSelector } from 'react-redux';
 export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
     await store.dispatch(setNavItems());
 });
 
 const TradingCenter_TWStocks_Self = function () {
+    const isMobile = useSelector(store => store.layout.isMobile);
     return (
         <>
             <PageHead title={'自選報價'} />
-            <div style={{ padding: '0 50px' }}>
+            <div style={{ padding: isMobile ? '0' : '0 30px' }}>
                 <NewWebIframe
                     iframeSrc={`/${process.env.NEXT_PUBLIC_NEWWEB}/TradingCenter_TWStocks_Self`}
                     title="永豐金證券"
