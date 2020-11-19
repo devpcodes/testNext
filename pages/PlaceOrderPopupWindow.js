bc8462a3ea929ea596c61df03a41e7005e73d1d3
wilson <sc.wilsonlin@gmail.com>
2020-11-19 14:09:47 +0800

    fix(placeOrderPopupWindow): 資料消失修正測試

--- pages/PlaceOrderPopupWindow.js
+++ pages/PlaceOrderPopupWindow.js
@@ -1,13 +1,27 @@
 import { useEffect, useRef } from 'react';
 import NewWebIframe from '../components/includes/NewWebIframe';
 import { PageHead } from '../components/includes/PageHead';
-
+import { checkServer } from '../services/checkServer';
 function PlaceOrderPopupWindow() {
     const iframeDom = useRef(null);
+    const timer = useRef(null);
+
+    if (!checkServer) {
+        timer.current = window.setInterval(setOpener, 200);
+    }
 
     useEffect(() => {
         iframeDom.current.contentWindow.opener = window.opener;
+        return () => {
+            window.clearInterval(timer.current);
+        };
     }, []);
+
+    const setOpener = () => {
+        if (iframeDom.current != null) {
+            iframeDom.current.contentWindow.opener = window.opener;
+        }
+    };
 
     const getIframeDom = dom => {
         iframeDom.current = dom;
