6c10abfa80052f636f9f3262869ce63a2d4ca81e
wilson <sc.wilsonlin@gmail.com>
2020-12-22 17:03:30 +0800

    fix(next.config.js): dev模式避免pwa警告

--- next.config.js
+++ next.config.js
@@ -1,47 +1,29 @@
 const withOptimizedImages = require('next-optimized-images');
 const withPWA = require('next-pwa');
-// const withImages = require('next-images');
-// const NextWorkboxPlugin = require('next-workbox-webpack-plugin');
-// const cache = require('./resources/serviceWorker/cache');
-// const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = withPWA(
-    withOptimizedImages({
+const isProd = process.env.NODE_ENV === 'production';
+if (isProd) {
+    module.exports = withPWA(
+        withOptimizedImages({
+            basePath: '/newweb',
+            assetPrefix: '/newweb',
+            pwa: {
+                disable: process.env.NODE_ENV === 'development',
+                dest: 'public',
+                register: false,
+                subdomainPrefix: '/newweb',
+            },
+            webpack: (config, { isServer, buildId, dev }) => {
+                return config;
+            },
+        }),
+    );
+} else {
+    module.exports = withOptimizedImages({
         basePath: '/newweb',
         assetPrefix: '/newweb',
-        pwa: {
-            disable: process.env.NODE_ENV === 'development',
-            dest: 'public',
-            register: false,
-            subdomainPrefix: '/newweb',
-        },
-        // inlineImageLimit: isProd ? 16384 : 0,
         webpack: (config, { isServer, buildId, dev }) => {
-            // let workboxOptions = {};
-            // if (isProd) {
-            //     workboxOptions = {
-            //         clientsClaim: true,
-            //         skipWaiting: true,
-            //         globPatterns: ['.next/static/*', '.next/static/commons/*'],
-            //         //無效
-            //         modifyUrlPrefix: {
-            //             '.next': '/newweb/_next',
-            //         },
-            //         runtimeCaching: cache,
-            //         globIgnores: ['**/webpack-hmr'],
-            //         swURLRoot: '/newweb/static/workbox',
-            //     };
-            // }
-
-            // if (!isServer && !dev) {
-            //     config.plugins.push(
-            //         new NextWorkboxPlugin({
-            //             buildId,
-            //             ...workboxOptions,
-            //         }),
-            //     );
-            // }
             return config;
         },
-    }),
-);
+    });
+}
