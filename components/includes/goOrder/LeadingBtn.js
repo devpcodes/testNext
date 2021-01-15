92b811793262d3c558b15e3864366b9ad5f08bb7
wilson <sc.wilsonlin@gmail.com>
2021-01-15 17:08:26 +0800

    feat(LeadingBtn): 買賣按鈕

---
+++ components/includes/goOrder/LeadingBtn.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { useWindowSize } from '../../../hooks/useWindowSize';
+
+const LeadingBtn = ({ containerHeight }) => {
+    const [shadow, setShadow] = useState('0px -7px 6px -3px #C7C7C7');
+    const winSize = useWindowSize();
+    useEffect(() => {
+        if (winSize.height < containerHeight + 110) {
+            setShadow('0px -7px 6px -3px #C7C7C7');
+        } else {
+            setShadow('none');
+        }
+    }, [winSize]);
+    return (
+        <div className="container">
+            <button className="btn buy">買進</button>
+            <button className="btn sell">賣出</button>
+            <style jsx>{`
+                .container {
+                    padding: 16px;
+                    width: 100%;
+                    position: fixed;
+                    bottom: 0;
+                    left: 0;
+                    background-color: white;
+                    z-index: 10000;
+                    box-shadow: ${shadow};
+                    -webkit-box-shadow: ${shadow};
+                    -moz-box-shadow: ${shadow};
+                    -o-box-shadow: ${shadow};
+                }
+                .btn {
+                    padding: 0;
+                    border: none;
+                    font: inherit;
+                    color: inherit;
+                    background-color: transparent;
+                    cursor: pointer;
+                    width: calc(50% - 8px);
+                    height: 60px;
+                    color: white;
+                    font-size: 24px;
+                    font-weight: bold;
+                    border-radius: 2px;
+                }
+                .buy {
+                    background-color: #f45a4c;
+                    margin-right: 8px;
+                }
+                .sell {
+                    background-color: #22a16f;
+                    margin-left: 8px;
+                }
+            `}</style>
+        </div>
+    );
+};
+export default LeadingBtn;
--- pages/OrderGO.js
+++ pages/OrderGO.js
@@ -5,10 +5,11 @@ import { Info } from '../components/includes/goOrder/infoArea/Info';
 import { Header } from '../components/includes/goOrder/header/Header';
 import SolaceClientComponent from '../components/includes/SolaceClientComponent';
 import QuoteContainer from '../components/includes/goOrder/quotes/QuoteContainer';
+import LeadingBtn from '../components/includes/goOrder/LeadingBtn';
 
 const OrderGO = () => {
     const [topic, setTopic] = useState([]);
-    const [title, setTitle] = useState('Order GO');
+    const [containerHeight, setContainerHeight] = useState(0);
     useEffect(() => {
         setTimeout(() => {
             setTopic(['MKT/*/*/2330', 'QUT/*/*/2330', 'SNP/*/*/2330']);
@@ -19,18 +20,28 @@ const OrderGO = () => {
         setTimeout(() => {
             setTopic(['MKT/*/*/2345', 'QUT/*/*/2345', 'SNP/*/*/2345']);
         }, 4000);
-        setTimeout(() => {
-            setTitle('aaaaa');
-        }, 9000);
+
+        window.addEventListener('resize', handleResize);
+        let elHeight = document.getElementById('container').clientHeight;
+        setContainerHeight(elHeight);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
+    const handleResize = () => {
+        let elHeight = document.getElementById('container').clientHeight;
+        setContainerHeight(elHeight);
+    };
     return (
         <>
-            <PageHead title={'快速下單'} />
-            <Header />
-            <Info />
-            <SolaceClientComponent subscribeTopic={topic} only={true} />
-            <QuoteContainer />
-            <h1>{title}</h1>
+            <div id="container">
+                <PageHead title={'快速下單'} />
+                <Header />
+                <Info />
+                <SolaceClientComponent subscribeTopic={topic} only={true} />
+                <QuoteContainer />
+            </div>
+            <LeadingBtn containerHeight={containerHeight} />
             <style jsx>{``}</style>
         </>
     );
