e63e719b63c802369deaea94fd4803250878e4cb
wilson <sc.wilsonlin@gmail.com>
2021-01-25 13:58:48 +0800

    feat(solaceClientComponent): 零股功能

--- pages/OrderGO.js
+++ pages/OrderGO.js
@@ -14,15 +14,6 @@ const OrderGO = () => {
     const code = useSelector(store => store.goOrder.code);
 
     useEffect(() => {
-        // setTimeout(() => {
-        //     setTopic(['MKT/*/*/2330', 'QUT/*/*/2330', 'SNP/*/*/2330']);
-        // }, 2000);
-        // setTimeout(() => {
-        //     setTopic(['MKT/*/*/0050', 'QUT/*/*/0050', 'SNP/*/*/0050']);
-        // }, 3000);
-        // setTimeout(() => {
-        //     setTopic(['MKT/*/*/2345', 'QUT/*/*/2345', 'SNP/*/*/2345']);
-        // }, 4000);
         window.addEventListener('resize', handleResize);
         return () => {
             window.removeEventListener('resize', handleResize);
