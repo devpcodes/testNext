eb75be36b19a6861a333611d08336eed496fe3c9
wilson <sc.wilsonlin@gmail.com>
2021-02-02 11:27:30 +0800

    feat(PriceControl): 金額

--- components/includes/goOrder/panel/PriceControl.js
+++ components/includes/goOrder/panel/PriceControl.js
@@ -1,32 +1,105 @@
+import { useState, useEffect, useRef } from 'react';
 import { Select, Input, Button } from 'antd';
 import { useSelector } from 'react-redux';
 import selectIcon from '../../../../resources/images/components/goOrder/arrow-chevron-down_black.png';
 import { themeColor } from './PanelTabs';
+import { checkServer } from '../../../../services/checkServer';
+import { formatPrice } from '../../../../services/numFormat';
+import { getStockPriceRange, getStockType } from '../../../../services/stockTickType';
 
 const { Option } = Select;
 const PriceControl = ({ title }) => {
+    const currentCode = useRef('');
+    const currentLot = useRef('');
+
     const bs = useSelector(store => store.goOrder.bs);
+    const code = useSelector(store => store.goOrder.code);
+    const lot = useSelector(store => store.goOrder.lot);
+    const solaceData = useSelector(store => store.solace.solaceData);
+    const [ordPrice, setOrderPrice] = useState('');
+    const [priceTypeOption, setPriceTypeOption] = useState([
+        { txt: '限價', val: ' ' },
+        { txt: '市價', val: '4' },
+        { txt: '漲停', val: '2' },
+        { txt: '跌停', val: '3' },
+        { txt: '平盤', val: '1' },
+    ]);
+    useEffect(() => {
+        // 整零切換先清空價格
+        if (lot !== currentLot.current) {
+            setOrderPrice('');
+        }
+        if (code === currentCode.current && lot === currentLot.current) {
+            return;
+        }
+
+        setPriceHandler();
+    }, [code, lot, solaceData]);
+
+    const setPriceHandler = () => {
+        if (lot === 'Odd') {
+            if (code !== '' && !checkServer() && solaceData.length > 0 && solaceData[0].data.OddlotClose != null) {
+                if (solaceData[0].topic.indexOf(code) >= 0 && solaceData[0].topic.indexOf('ODDLT')) {
+                    currentCode.current = code;
+                    currentLot.current = lot;
+                    setOrderPrice(formatPrice(solaceData[0].data.OddlotClose));
+                }
+            }
+        } else {
+            if (code !== '' && !checkServer() && solaceData.length > 0 && Array.isArray(solaceData[0].data.Close)) {
+                if (solaceData[0].topic.indexOf(code) >= 0) {
+                    currentCode.current = code;
+                    currentLot.current = lot;
+                    setOrderPrice(formatPrice(solaceData[0].data.Close[0]));
+                }
+            }
+        }
+    };
+
+    const priceChangeHandler = e => {
+        setOrderPrice(e.target.value);
+    };
+
+    const onClickHandler = symbol => {
+        const type = getStockType(code).type;
+        let unit;
+        if (symbol === '+') {
+            unit = getStockPriceRange(type, ordPrice, true);
+            setOrderPrice(formatPrice(parseFloat(ordPrice) + unit));
+        } else {
+            unit = getStockPriceRange(type, ordPrice, true);
+            if (parseFloat(ordPrice) - unit <= unit) {
+                setOrderPrice(formatPrice(unit));
+            } else {
+                setOrderPrice(formatPrice(parseFloat(ordPrice) - unit));
+            }
+        }
+    };
     return (
         <div className="price_control">
             <div className="select__box">
                 {title === '限價' ? (
                     <Select value=" " suffixIcon={<img src={selectIcon} />}>
-                        <Option value=" ">限價</Option>
-                        <Option value="4">市價</Option>
-                        <Option value="2">漲停</Option>
-                        <Option value="3">跌停</Option>
-                        <Option value="1">平盤</Option>
+                        {priceTypeOption.map((item, index) => {
+                            return (
+                                <Option key={index} value={item.val}>
+                                    {item.txt}
+                                </Option>
+                            );
+                        })}
                     </Select>
                 ) : (
                     <div className="select__label">{title}</div>
                 )}
             </div>
             <div className="input__box">
-                <Input />
+                <Input value={title === '限價' ? ordPrice : ''} onChange={priceChangeHandler} />
             </div>
             <div className="btn__box">
-                <Button>-</Button>
-                <Button style={{ marginLeft: '8px' }}>+</Button>
+                <Button onClick={onClickHandler.bind(null, '-')}>-</Button>
+                <Button onClick={onClickHandler.bind(null, '+')} style={{ marginLeft: '8px' }}>
+                    +
+                </Button>
             </div>
             <style jsx>{``}</style>
             <style global jsx>{`
