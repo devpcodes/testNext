0804554a83a746334be6d215d24cc0904916f226
Shane Huang <shane.huang@sinopac.com>
2021-02-04 17:39:51 +0800

    feat(OrderGO): fetch popular stocks

--- services/components/goOrder/productFetcher.js
+++ services/components/goOrder/productFetcher.js
@@ -1,5 +1,6 @@
 import { getLykanInstance } from '../../myAxios';
 
+// 商品搜尋
 export const fetchProducts = async function ({
     query,
     marketType = ['S', 'SB', 'F', 'O'],
@@ -13,5 +14,12 @@ export const fetchProducts = async function ({
         limit,
         isOrder, // 是否查詢可下單的商品
     });
+    return res.data;
+};
+
+// 台股熱門商品
+export const fetchPopularStocks = async () => {
+    const url = `/product/getStockPopularItem`;
+    const res = await getLykanInstance().get(url);
     return res.data;
 };
