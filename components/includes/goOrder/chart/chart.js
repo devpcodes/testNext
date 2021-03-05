1b7175c920be5e042b9303517ce513aea5b8f346
Mona Lisa <bryan.cheng@sinopac.com>
2021-03-05 16:53:39 +0800

    (feat) 漲跌停顏色、時間區間調整、整零股切換自適應寬度調整、浮水印測試。

--- components/includes/goOrder/chart/chart.js
+++ components/includes/goOrder/chart/chart.js
@@ -7,6 +7,16 @@ import { fetchStockMinuteKline } from '../../../../services/stock/stockMinuteKli
 
 am4core.useTheme(am4themes_animated);
 am4core.addLicense(process.env.NEXT_PUBLIC_AMCHART_LICENSE);
+let watermark;
+
+// setTimeout(function(){
+//     let chart = am4core.registry.baseSprites.find(c => c.htmlContainer.id === 'chartdiv');
+//     console.log(chart.plotContainer.children.indexOf(watermark))
+//     console.log(chart.plotContainer.children.getIndex(7))
+//     let a = chart.plotContainer.children.getIndex(7)
+//     a.text = "ddddddddd"
+// }, 5000)
+
 const Chart = function () {
     const code = useSelector(store => store.goOrder.code);
     const lot = useSelector(store => store.goOrder.lot);
@@ -16,6 +26,7 @@ const Chart = function () {
         const res = await fetchStockMinuteKline(code);
         setKline(res);
     };
+
     // 抓取 kLine 資料
     useEffect(() => {
         if (code !== '') {
@@ -26,14 +37,21 @@ const Chart = function () {
 
     // 接上 solace 資料
     useEffect(() => {
+        let chart = am4core.registry.baseSprites.find(c => c.htmlContainer.id === 'chartdiv');
+        if (chart) {
+        }
         if (solaceData && solaceData.length > 0) {
+            // console.log(chart)
+            // StopTrade = 1
+            // watermark.text = '盤前試撮'
+            if (solaceData[0].OddlotSimtrade === 1 || solaceData[0].Simtrade === 1) {
+            }
             if (solaceData[0].topic.indexOf('MKT') !== -1) {
                 const time =
                     lot === 'Board'
                         ? `${solaceData[0].data.Date} ${solaceData[0].data.Time}`
                         : `${solaceData[0].data.Date} ${solaceData[0].data.OddlotTime}`;
                 const close = lot === 'Board' ? solaceData[0].data.Close[0] : solaceData[0].data.OddlotClose;
-                let chart = am4core.registry.baseSprites.find(c => c.htmlContainer.id === 'chartdiv');
                 let chartData = {
                     ts: new Date(time),
                     Close: close,
@@ -44,13 +62,18 @@ const Chart = function () {
     }, [solaceData]);
 
     useEffect(() => {
+        let chart = am4core.registry.baseSprites.find(c => c.htmlContainer.id === 'chartdiv');
+        if (chart) {
+            chart.dispose();
+        }
         drawChart();
     }, [kline]);
 
     const drawChart = () => {
+        console.log(kline);
         if (!_.isEmpty(kline)) {
             let chart = am4core.create('chartdiv', am4charts.XYChart);
-
+            console.log(11111);
             if (kline.OHCL.length > 0) {
                 kline.OHCL.map(function (a, b) {
                     a.ts = new Date(a.ts);
@@ -62,11 +85,11 @@ const Chart = function () {
             if (Array.isArray(chart.data) && chart.data.length) {
                 let firstTick = Object.assign({}, chart.data[0]);
                 firstTick.ts = new Date(chart.data[0].ts).setHours(9, 0, 0, 0);
-                firstTick.Close = kline.Reference;
+                firstTick.Close = kline.Open;
                 chart.data.unshift(firstTick);
             }
 
-            // 時間軸設定
+            // 時間軸設定 (resizable)
             let timeAxis = chart.xAxes.push(new am4charts.DateAxis());
             timeAxis.dataFields.category = 'time';
             timeAxis.fontSize = 12;
@@ -74,18 +97,19 @@ const Chart = function () {
             timeAxis.baseInterval = { timeUnit: 'second', count: 1 };
             timeAxis.renderer.grid.template.location = 0;
             timeAxis.renderer.labels.template.location = 0.0001; // 不可設0
+            timeAxis.cursorTooltipEnabled = false;
             timeAxis.renderer.minGridDistance = 1; // 手機板刻度固定
-            // timeAxis.minZoomCount = 1;
             timeAxis.min = new Date().setHours(9, 0, 0, 0);
             timeAxis.max = new Date().setHours(13, 30, 0, 0);
             timeAxis.groupData = true;
             timeAxis.groupCount = 1500;
-            timeAxis.cursorTooltipEnabled = false;
             timeAxis.gridIntervals.setAll([
                 { timeUnit: 'hour', count: 1 },
                 { timeUnit: 'minute', count: 1 },
                 { timeUnit: 'second', count: 1 },
             ]);
+
+            timeAxis.dateFormats.setKey('hour', 'HH');
 
             // 價格軸設定
             let priceAxis = chart.yAxes.push(new am4charts.ValueAxis());
@@ -98,9 +122,18 @@ const Chart = function () {
             priceAxis.fontSize = 12;
             priceAxis.tooltip.label.fontSize = 10;
             priceAxis.cursorTooltipEnabled = false;
+            priceAxis.minGridDistance = 30;
 
-            // priceAxis.includeRangesInMinMax = true
-            // priceAxis.calculateTotals = true;
+            priceAxis.renderer.labels.template.adapter.add('text', (label, target, key) => {
+                if (label > kline.Reference) {
+                    return '[#f00]' + label;
+                } else if (label < kline.Reference) {
+                    return '[#22a16f]' + label;
+                } else {
+                    return label;
+                }
+            });
+
             // 線圖設定
             let series = chart.series.push(new am4charts.LineSeries());
             series.dataFields.dateX = 'ts';
@@ -108,7 +141,31 @@ const Chart = function () {
             series.tooltipText = '{valueY.value}';
             series.tooltip.label.fontSize = 10;
             series.numberFormatter.numberFormat = '#.00';
+
+            series.tooltip.getFillFromObject = false;
+            series.tooltip.background.fill = am4core.color('#000');
+            series.tooltip.label.fill = am4core.color('#fff');
+
+            series.adapter.add('tooltipText', function (ev, b) {
+                var text = '[bold, #fff]{valueY}[/]\n';
+                return text;
+            });
+
             chart.cursor = new am4charts.XYCursor();
+
+            // waterMark
+            watermark = new am4core.Label();
+            watermark.text = 'TEST';
+            watermark.align = 'left';
+            watermark.valign = 'top';
+            watermark.fontSize = 20;
+            watermark.opacity = 0.3;
+            watermark.marginTop = 5;
+            watermark.marginLeft = 5;
+            watermark.fontWeight = 600;
+            watermark.id = 'watermark';
+            watermark.text = '1231242354';
+            chart.plotContainer.children.push(watermark);
 
             // 漲區塊設定
             let uRange = priceAxis.createSeriesRange(series);
@@ -118,17 +175,13 @@ const Chart = function () {
             uRange.contents.fill = am4core.color('#f00');
             uRange.contents.fillOpacity = 0.1;
 
-            // 跌區塊設定
+            // // 跌區塊設定
             let dRange = priceAxis.createSeriesRange(series);
             dRange.value = kline.DownLimit; // 跌停
             dRange.endValue = kline.Reference; // 平盤價
             dRange.contents.stroke = am4core.color('#22a16f');
-            dRange.contents.fill = am4core.color('#000');
+            dRange.contents.fill = am4core.color('#22a16f');
             dRange.contents.fillOpacity = 0.1;
-
-            return () => {
-                chart.dispose();
-            };
         }
     };
 
