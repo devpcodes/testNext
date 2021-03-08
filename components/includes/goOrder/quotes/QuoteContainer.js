15a9a504d3432898edd26ad1b9ab0384ecd07afc
wilson <sc.wilsonlin@gmail.com>
2021-03-08 15:39:33 +0800

    feat(QuoteContainer): 上折高度調整

--- components/includes/goOrder/quotes/QuoteContainer.js
+++ components/includes/goOrder/quotes/QuoteContainer.js
@@ -42,7 +42,7 @@ const QuoteContainer = () => {
         if (panelHeight >= 100 && bs !== '') {
             quoteContainerElement.current.scrollTop = 0;
             return {
-                transform: 'translateY(-310px)', //-60
+                transform: 'translateY(-220px)', //-60
             };
         }
     };
