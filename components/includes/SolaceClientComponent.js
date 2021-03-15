074da5ddabe3c32dd44906ffe912c5dbf0f0553b
wilson <sc.wilsonlin@gmail.com>
2021-03-15 11:56:15 +0800

    test

--- components/includes/SolaceClientComponent.js
+++ components/includes/SolaceClientComponent.js
@@ -43,8 +43,7 @@ const SolaceClientComponent = ({ subscribeTopic, idno }) => {
     const solaceLoadedHandler = () => {
         if (solace.current == null) {
             // console.log('============ID', idno);
-            solace.current = solaceClient('', getCookie('user_id'));
-            // solace.current = solaceClient('', idno);
+            solace.current = solaceClient('', idno);
             solace.current.connect();
             solace.current.setMessageEvent('ST', function (xhr) {
                 console.log('solace', xhr);
