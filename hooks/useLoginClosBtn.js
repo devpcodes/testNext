90d9c69ec6de02c02a601afdb946e9e88843b312
wilson <sc.wilsonlin@gmail.com>
2021-04-09 16:11:32 +0800

    feat(login,sinotradeLogin): 配合上版順調，暫時關閉第三方登入

--- hooks/useLoginClosBtn.js
+++ hooks/useLoginClosBtn.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import { usePlatform } from './usePlatform';
 
 //一些特別處理的platform
-export const noCloseBtns = [];
+//TODO 配合上版順序調整
+export const noCloseBtns = ['udn'];
 export const useLoginClosBtn = () => {
     const platform = usePlatform();
     const [noCloseBtn, setNoCloseBtn] = useState(false);
