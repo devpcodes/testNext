6c9c13194f3dbb8d152f1300011aeb79306a9ef1
wilson <sc.wilsonlin@gmail.com>
2021-05-07 12:16:54 +0800

    feat(postApplyEarmarkReserve): 錯誤處理

--- services/components/reservationStock/postApplyEarmarkReserve.js
+++ services/components/reservationStock/postApplyEarmarkReserve.js
@@ -11,14 +11,28 @@ export const PostApplyEarmarkReserve = async ({
     ca_content,
 }) => {
     const url = '/stp/api/applyEarmarkReserve';
-    const res = await getA8StpInstance(true).post(url, {
-        account,
-        branch,
-        symbol,
-        order_qty,
-        order_price,
-        token,
-        ca_content,
-    });
-    console.log('res', res);
+    try {
+        const res = await getA8StpInstance(true).post(url, {
+            account,
+            branch,
+            symbol,
+            order_qty,
+            order_price,
+            token,
+            ca_content,
+        });
+        console.log('res', res);
+        if (res?.data?.success === true) {
+            return res.data?.result || '';
+        } else {
+            var err = res.data.message || '伺服器錯誤';
+            Modal.error({
+                content: err,
+            });
+            throw err;
+        }
+    } catch (error) {
+        console.log(error.message);
+        throw error;
+    }
 };
