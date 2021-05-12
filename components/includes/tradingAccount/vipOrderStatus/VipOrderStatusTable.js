ce062f2e246189a249a04451ac44a768149f801b
wilson <sc.wilsonlin@gmail.com>
2021-05-12 15:27:20 +0800

    fix(VipOrderStatusTable): 去除多餘的排序code

--- components/includes/tradingAccount/vipOrderStatus/VipOrderStatusTable.js
+++ components/includes/tradingAccount/vipOrderStatus/VipOrderStatusTable.js
@@ -93,15 +93,16 @@ const VipOrderStatusTable = ({ showDelBtn, controlReload, getSearchVal, getPageI
         let newData = [];
         if (Array.isArray(fetchData?.data)) {
             setError('');
-            newData = fetchData.data.sort((a, b) => {
-                if (a.ord_time.length <= 6) {
-                    a.ord_time += '000';
-                }
-                if (b.ord_time.length <= 6) {
-                    b.ord_time += '000';
-                }
-                return Number(b.ord_time) - Number(a.ord_time);
-            });
+            // newData = fetchData.data.sort((a, b) => {
+            //     if (a.ord_time.length <= 6) {
+            //         a.ord_time += '000';
+            //     }
+            //     if (b.ord_time.length <= 6) {
+            //         b.ord_time += '000';
+            //     }
+            //     return Number(b.ord_time) - Number(a.ord_time);
+            // });
+            newData = fetchData.data;
             newData = newData.map((item, index) => {
                 item.key = index;
                 item.platform = platform;
