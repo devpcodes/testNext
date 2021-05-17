20974a6b7a835d394c2616b2890a3978527f5719
Mona Lisa <bryan.cheng@sinopac.com>
2021-05-17 10:01:29 +0800

    自選暫存

--- components/includes/selfSelect/SelfSelectToolBar.js
+++ components/includes/selfSelect/SelfSelectToolBar.js
@@ -9,7 +9,7 @@ const SelfSelectToolBar = () => {
                     <h2>我的自選</h2>
                     <AddSelfSelect />
                 </div>
-                <div className="select__toolbar__left">
+                <div className="select__toolbar__right">
                     <span>共 12/50 檔自選股</span>
                     <Button type="primary" loading>
                         reload
@@ -19,6 +19,21 @@ const SelfSelectToolBar = () => {
                     </Button>
                 </div>
             </div>
+
+            <style jsx>{`
+                .select__toolbar {
+                    display: flex;
+                }
+                .select__toolbar__left {
+                    display: flex;
+                }
+                .select__toolbar__left > h2 {
+                    font-size: 2.8rem;
+                    font-weight: bold;
+                    margin: 0;
+                    padding: 0;
+                }
+            `}</style>
         </>
     );
 };
