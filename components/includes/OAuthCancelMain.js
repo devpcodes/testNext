b4fe66f645a75c7bd3cd28fe7c0d26ce7da98862
AnnLin <PUNKMOON0@gmail.com>
2021-06-15 09:54:36 +0800

    空值畫面處理

--- components/includes/OAuthCancelMain.js
+++ components/includes/OAuthCancelMain.js
@@ -5,6 +5,7 @@ import { getToken } from '../../services/user/accessToken';
 import { useDispatch, useSelector } from 'react-redux';
 import { setModal } from '../../store/components/layouts/action';
 import infoIcon from '../../resources/images/components/goOrder/attention-info-circle.svg';
+import noDataImg from '../../resources/images/components/oauthCancel/img-no-data.svg';
 
 const OAuthCancelMain = () => {
 const [dataSource, ds_getData] = useState([]); 
@@ -90,11 +91,10 @@ const testfunc = (e) =>{
                 </Tooltip>
             </div>
                 <div className="sub_text for_pc">這些是你使用永豐金證券帳號登入過的應用程式或網站，你可以自由地解除與他們之間的授權。</div>
-            
+            <ul className="item_list"> 
                  {    
                  dataSource.length>0 ?(
-                    <ul className="item_list"> 
-                    {dataSource.map( (x, i) => ( 
+                    dataSource.map( (x, i) => ( 
                        <li key={x.clientId}>
                         <div className="list_img"><img src={x.clientImage}></img></div> 
                         <div className="text_area">
@@ -103,15 +103,18 @@ const testfunc = (e) =>{
                         </div>
                         <div className="btn_box"><a className="btn_r" onClick={(e) => cancel({id:x.clientId, name:x.clientName}, e)}>解除<span>授權</span></a></div>
                       </li>)
-                   )}
-                   </ul>
-                   ):null
+                   )
+                   ):(
+                       <div className="noDataBox">
+                          <img src={noDataImg}></img>
+                          <div className="noDataTxt">目前暫無資料</div>
+                       </div>
+                   )
                    }
-            
+            </ul>
             </div>
             <style jsx>{`
-
-            .content_box{max-width:776px;margin: 0px auto; padding-top:100px;}
+            .content_box{max-width:776px;min-height:700px;margin: 0px auto; padding-top:100px;}
             .list_img{width:68px;height:68px;flex-shrink: 0;padding:5px; margin:0 20px 0 0;  border-radius: 2px; 
                 border: solid 1px #d7e0ef;overflow:hidden;}
             .list_img img{width:100%;}   
@@ -138,7 +141,7 @@ const testfunc = (e) =>{
                 font-size: 16px; font-weight: 700; font-stretch: normal;
                 font-style: normal;  line-height: normal; letter-spacing: 0.4px;color: #6c7b94
                 }
-            .item_list{ width:776px;   margin: 28px 0 0;
+            .item_list{ width:776px;   margin: 28px 0 4em;
                 padding:0; list-style:none; border-radius: 2px;
                 border: solid 1px #d7e0ef; background-color: #ffffff;
             }
@@ -146,9 +149,11 @@ const testfunc = (e) =>{
                 padding: 20px; border-bottom: solid 1px #d7e0ef; display:flex
             }
             .item_list li:last-child{ border-bottom: none; }
+            .noDataBox{text-align:center;padding:60px 0;}
+            .noDataBox .noDataTxt{text-align:center;font-size:16px;color:#3f5372;margin-top:1.5em;}
             .for_m{display:none;}
             @media(max-width:768px){
-                .content_box{width:100%;padding:0}
+                .content_box{width:100%;padding:0;min-height:0px}
                 .content_box{}
                 .list_img{width:52px;height:52px;}
                 .text_area{ width: calc(100% - 105px);}
@@ -157,6 +162,9 @@ const testfunc = (e) =>{
                 .item_list {width:100%;margin:0;border-width:1px 0;border-radius:0;}
                 .item_list li{padding:16px}
                 .btn_r span{display:none}
+                .noDataBox{padding:40px 0;}
+                .noDataBox img{width:100px}
+                .noDataBox .noDataTxt{margin-top:1em;}
                 .for_pc{display:none;}
                 .for_m{display:inherit;}
             }
