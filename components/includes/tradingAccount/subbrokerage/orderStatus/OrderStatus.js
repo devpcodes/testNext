e76edfb2d6b861ba74c3d3b6942e4ac2fd545db3
wilson <sc.wilsonlin@gmail.com>
2021-07-22 11:13:08 +0800

    wait

--- components/includes/tradingAccount/subbrokerage/orderStatus/OrderStatus.js
+++ components/includes/tradingAccount/subbrokerage/orderStatus/OrderStatus.js
@@ -1,8 +1,23 @@
 import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import useSWR from 'swr';
 const OrderStatus = () => {
-    useEffect(() => {
-        getData();
-    }, []);
+    const currentAccount = useSelector(store => store.user.currentAccount);
+    // const { data: fetchData } = useSWR(
+    //     [JSON.stringify(postData)],
+    //     orderStatusQueryFetcherWithSWR,
+    //     {
+    //         onError: (error, key) => {
+    //             Modal.error({
+    //                 title: '伺服器錯誤',
+    //             });
+    //             setError('伺服器錯誤');
+    //         },
+    //         errorRetryCount: 3,
+    //         focusThrottleInterval: 10000,
+    //         errorRetryInterval: 10000,
+    //     },
+    // );
     const getData = async () => {};
     return <div>123</div>;
 };
