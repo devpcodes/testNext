14318564ab6eb65ece84c3b81063bf683a96dd21
wilson <sc.wilsonlin@gmail.com>
2021-09-14 10:13:10 +0800

    feat(ActiveReturn): 期貨回報

--- components/includes/ActiveReturn.js
+++ components/includes/ActiveReturn.js
@@ -8,16 +8,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setWebSocketInfo } from '../../store/activeReturn/action';
 import { fetchOneContracts } from '../../services/stock/fetchOneContracts';
 import { postSbcoCode } from '../../services/components/goOrder/sb/postSbcoCode';
-import { throttle } from '../../services/throttle';
+import { throttle, debounce } from '../../services/throttle';
+import { postStmbCode } from '../../services/future/postStmbCode';
+
 // import { fetchOneContracts } from '../../services/stock/fetchOneContracts';
 
 var myWebsocket;
+
+// 配合我們的solace過多的呼叫次數及原newweb 改寫...
 const ActiveReturn = () => {
     const dispatch = useDispatch();
     const websocketInfo = useSelector(store => store.activeReturn?.websocketInfo);
 
     const stmbCodeList_future = useRef([]);
     const currResult = useRef('');
+    // const currSocket = useRef({});
     useEffect(() => {
         if (getCookie('accounts')) {
             myWebsocket = webSocketLogin(getCookie('accounts'));
@@ -39,7 +44,8 @@ const ActiveReturn = () => {
         try {
             const socketData = JSON.parse(e.data);
             const topicArgs = socketData.topic.split('/');
-            // console.log('-------', socketData, topicArgs);
+
+            console.log('-------', socketData, topicArgs);
             if (socketData.topic.indexOf('TFT') >= 0) {
                 openNotification('topRight', socketData);
             }
@@ -56,8 +62,14 @@ const ActiveReturn = () => {
                 }
             }
             if (topicArgs[2] === 'F') {
-                futureOptionsHandler(socketData);
-                // openNotificationF('topRight', socketData);
+                // 一段時間內只執行最後一次的呼叫
+                debounce(futureOptionsHandler.bind(null, socketData, topicArgs[3]), 1000);
+                // if(currSocket.current == JSON.stringify(socketData)) {
+                //     throttle(futureOptionsHandler.bind(null, socketData, topicArgs[3]), 1000)
+                // }else{
+                //     futureOptionsHandler(socketData, topicArgs[3]);
+                //     currSocket.current = socketData;
+                // }
             }
             dispatch(setWebSocketInfo(socketData));
         } catch (err) {
@@ -65,12 +77,12 @@ const ActiveReturn = () => {
         }
     });
 
-    const futureOptionsHandler = socketData => {
+    const futureOptionsHandler = (socketData, Oore) => {
+        console.log('ffff', socketData);
         switch (socketData.COMTYPE) {
             case '0':
             case '2':
-                console.log('ffff', socketData);
-                openNotificationF('topRight', socketData);
+                openNotificationF('topRight', socketData, Oore);
                 break;
 
             case '1':
@@ -239,11 +251,30 @@ const ActiveReturn = () => {
             return <span>{socketData.BS === 'B' ? '買' : '賣'}</span>;
         }
     };
-    //message就是socketdata
-    const getFuturesInfo = message => {
-        message.category = message.COMNO = message.COMNO.trim();
-        message.COMYM = message.COMYM.trim();
-        switch (message.COMNO) {
+
+    const getBS = BS => {
+        switch (BS) {
+            case 'B':
+                return { name: '買進' };
+            case 'S':
+                return { name: '賣出' };
+        }
+    };
+
+    const getOorE = OroE => {
+        switch (OroE) {
+            case 'O':
+                return '委';
+            case 'E':
+                return '成';
+        }
+    };
+
+    const getFutureCategory = message => {
+        message.COMNO = message.COMNO?.trim();
+        message.category = message.COMNO?.trim();
+        message.COMYM = message.COMYM?.trim();
+        switch (message.COMNO.trim()) {
             case 'FITE':
                 message.category = 'EXF';
                 break;
@@ -280,6 +311,84 @@ const ActiveReturn = () => {
                     default:
                         message.category = message.COMNO;
                 }
+        }
+        console.log('why............', message.category, message.COMNO);
+        return message.category;
+    };
+    //message就是socketdata
+    const getFuturesInfo = async message => {
+        const category = getFutureCategory(message);
+        let cateObj = {};
+        try {
+            const res = await postStmbCode();
+            console.log('res', res);
+            stmbCodeList_future.current = res.content;
+            console.log('========', stmbCodeList_future.current[category], stmbCodeList_future.current, category);
+            if (stmbCodeList_future.current.hasOwnProperty(message.category)) {
+                cateObj = stmbCodeList_future.current[message.category].find(function (d) {
+                    return d.deliverymonth == message.COMYM.trim();
+                });
+                if (message.category == 'MXF') {
+                    message.category = !!cateObj.ename ? cateObj.ename : message.category;
+                }
+            }
+
+            return {
+                category: message.category,
+                name: (!!cateObj ? cateObj.csname + ' ' : '') + message.category + ' ' + message.COMYM + ' 月 ',
+                msg: message,
+            };
+        } catch (error) {
+            throw error?.message;
+        }
+    };
+
+    const getFuturesResult = (code, OorE) => {
+        if (OorE == 'O') {
+            switch (code) {
+                case '0000':
+                    return '委託成功';
+                case '8014':
+                    return '委託成功';
+                default:
+                    return '委託失敗';
+            }
+        } else {
+            return '成交';
+        }
+    };
+
+    const getOPOF = OPOF => {
+        switch (OPOF) {
+            case '0':
+                return '新倉';
+            case '1':
+                return '平倉';
+            case '7':
+                return '代沖銷';
+            default:
+                return '自動';
+        }
+    };
+
+    const getFuturesOrderType = message => {
+        switch (message.ORDTYPE) {
+            case 'CXL':
+                return '刪單';
+            case 'UPD':
+                return '改量';
+            case 'UPL':
+                return '改限價';
+            case 'UPM':
+                return '改市價';
+            case 'UPP':
+                return '改一定範圍市價';
+            case 'CXP':
+                return '退單';
+            case 'CXQ':
+                return '退單基準價';
+            default:
+                return '';
         }
     };
 
@@ -402,8 +511,62 @@ const ActiveReturn = () => {
         }
     };
 
-    const openNotificationF = socketData => {
-        // getFuturesInfo(socketData);
+    const openNotificationF = async (placement, socketData, Oore) => {
+        const info = await getFuturesInfo(socketData);
+        socketData.startText = '';
+        if (getFuturesOrderType(socketData) == '刪單') socketData.startText = '取消';
+        console.log('info', info, socketData, getFuturesResult(socketData.CODE, getOorE(Oore)), Oore);
+        const message =
+            getFuturesResult(socketData.CODE, Oore) +
+            ' ' +
+            getOPOF(socketData.OPOF) +
+            getFuturesOrderType(socketData) +
+            ' ' +
+            socketData.startText +
+            getBS(socketData.PS).name;
+        socketData.BS = socketData.PS;
+        const icon = getIcon(message, socketData);
+        console.log('oore', Oore);
+        if (getOorE(Oore) == '成') {
+            socketData.PRICE1 = parseFloat(socketData.TRDPRC1); //價格
+            socketData.ORDQTY = parseInt(socketData.TRDQTY); //數量
+        }
+        notification.info({
+            className: 'activeReturn',
+            message: message,
+            description: socketData?.ERRMSG?.trim()
+                ? decodeURIComponent(escape(socketData.ERRMSG))
+                : info.name + ' / ' + parseFloat(socketData?.PRICE1) + ' / ' + parseInt(socketData?.ORDQTY) + '口',
+
+            placement,
+            style: {
+                background: socketData.PS === 'B' ? themeColor.buyTabColor : themeColor.sellTabColor,
+                width: '300px',
+            },
+            top: '59px',
+            icon: (
+                <div
+                    style={{
+                        background: 'white',
+                        width: '45px',
+                        height: '45px',
+                        textAlign: 'center',
+                        paddingTop: '11px',
+                        fontSize: '32px',
+                        fontWeight: 'bold',
+                        borderRadius: '2px',
+                        marginLeft: '-12px',
+                        color: `${socketData.PS === 'B' ? themeColor.buyTabColor : themeColor.sellTabColor}`,
+                        boxShadow: 'rgb(123 123 123 / 18%) 4px 4px 3px 0px',
+                    }}
+                >
+                    {icon}
+                </div>
+            ),
+            onClose: () => {
+                dispatch(setWebSocketInfo({}));
+            },
+        });
     };
 
     return (
--- services/throttle.js
+++ services/throttle.js
@@ -2,6 +2,7 @@ import moment from 'moment';
 
 var lastTime = 0;
 var nowTime = 0;
+
 /**
  * 至少delay多少毫秒才執行的函数
  * @param fn  要執行的函数
@@ -13,4 +14,17 @@ export const throttle = (fn, delay) => {
         fn.call(this);
         lastTime = nowTime;
     }
+};
+
+var timer = null;
+/**
+ * 一段時間內只執行最後一次的呼叫
+ * @param fn 呼叫的function
+ * @param delay 延遲時間
+ */
+export const debounce = (fn, delay) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        fn.call(this);
+    }, delay);
 };
