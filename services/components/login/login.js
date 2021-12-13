64b652697f4dcfdb3ce7c849592a355c7fb7f10a
wilson <sc.wilsonlin@gmail.com>
2021-12-13 10:06:27 +0800

    fix: 衝突

--- services/components/login/login.js
+++ services/components/login/login.js
@@ -3,18 +3,18 @@ import { setToken } from '../../user/accessToken';
 
 export const submit = async function (account, password, reCAPTCHAToken, version) {
     // login 有設置 cookie 的作用。因同源政策，由 nginx 將 lykan 設為同源，待舊站完全下架 (不再採用 cookie) 時，改發 https://servicerd.sinotrade.com.tw/lykan/api/v1/auth/login
-    const data = {
-        user_id: account,
-        password,
-        // 'g-recaptcha-response': reCAPTCHAToken,
-        platform: 'newweb',
-    };
-    if (reCAPTCHAToken != null) {
-        data['g-recaptcha-response'] = reCAPTCHAToken;
-    }
-    if (captcha != null) {
-        data['captcha'] = captcha;
-    }
+    // const data = {
+    //     user_id: account,
+    //     password,
+    //     // 'g-recaptcha-response': reCAPTCHAToken,
+    //     platform: 'newweb',
+    // };
+    // if (reCAPTCHAToken != null) {
+    //     data['g-recaptcha-response'] = reCAPTCHAToken;
+    // }
+    // if (captcha != null) {
+    //     data['captcha'] = captcha;
+    // }
 
     const res = await axios({
         method: 'post',
