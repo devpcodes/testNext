a10810534a19e61d6cf017f829ca7ffeb3c83ac0
Mona Lisa <bryan.cheng@sinopac.com>
2021-12-16 13:57:06 +0800

    CA TEST

--- services/webCa.js
+++ services/webCa.js
@@ -340,12 +340,12 @@ export const CAHandler = async function (token, cb) {
                     // 清除台網母憑證
                     window.open('https://catest.sinotrade.com.tw/WebCA/clearLS.html');
 
-                    // 刪除 local 憑證
-                    for (var key in localStorage) {
-                        if (/^TWCA/.test(key) === true) {
-                            localStorage.removeItem(key);
-                        }
-                    }
+                    // // 刪除 local 憑證
+                    // for (var key in localStorage) {
+                    //     if (/^TWCA/.test(key) === true) {
+                    //         localStorage.removeItem(key);
+                    //     }
+                    // }
 
                     // 重新部署憑證
                     caResultDataHandler('ApplyCert', tokenVal.user_id, token, cb, function () {
