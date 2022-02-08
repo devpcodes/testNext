5bafa76248cce2a5deedeca1e4c0744911158465
wilson <sc.wilsonlin@gmail.com>
2022-02-08 09:13:34 +0800

    feat(widget): 正式機DataAPIPath調整

--- widget/lsinopac/js/constant.js
+++ widget/lsinopac/js/constant.js
@@ -1,7 +1,7 @@
 var APP_CONFIG = {
     VERSION: "1.29.0",
    //  DataAPIPath: "https://t1www5.trkd-hs.com/sinopacwidget2", //staging
-     DataAPIPath: "/lykan/api/v1/labci/sinopacwidget", //staging
+     DataAPIPath: window.location.hostname.indexOf('webrd') >=0 ? "/lykan/api/v1/labci/sinopacwidget": 'https://hkhsm.trkd-hs.com/sinopacwidget', //staging
     //  DataAPIPath: "https://t1hkhsm.trkd-hs.com/sinopacwidget", //staging
     //DataAPIPath: "https://www5.trkd-hs.com/sinopacwidget", //prod
     WDS_MAX_IDLE_TIME: 60,
