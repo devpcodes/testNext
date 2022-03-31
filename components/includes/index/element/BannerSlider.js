34f8f56eb2998e8d318c00ef2f98a520e8d8a618
wilson <sc.wilsonlin@gmail.com>
2022-03-31 16:59:12 +0800

    fix(index): 廣告另開功能

--- components/includes/index/element/BannerSlider.js
+++ components/includes/index/element/BannerSlider.js
@@ -42,7 +42,7 @@ const BannerSlider = () => {
                     {Array.isArray(ads) &&
                         ads.map((e, i) => (
                             <div key={i}>
-                                <a href={e.url}>
+                                <a href={e.url} target={e.isBlank ? '_blank' : '_self'}>
                                     {/* <h3
                                         style={
                                             clientWidth > 450
