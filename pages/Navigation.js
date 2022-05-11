c9e0864d6707c358264e7c239cb24a68c0a96d9b
Mona Lisa <bryan.cheng@sinopac.com>
2022-05-11 10:56:37 +0800

    feat (NAVIGATION) 測試註銷憑證

--- pages/Navigation.js
+++ pages/Navigation.js
@@ -7,8 +7,8 @@ import { objectToQueryHandler } from '../services/objectToQueryHandler';
 import { useSessionStorage } from '../hooks/useSessionStorage';
 import { getToken } from '../services/user/accessToken';
 import { caValidator } from '../services/caValidator';
-
-import { checkCert, clearCert } from '../services/webCa';
+import CaHead from '../components/includes/CaHead';
+import { checkCert, clearCert, signCert } from '../services/webCa';
 
 const Navigation = () => {
     const router = useRouter();
@@ -30,68 +30,71 @@ const Navigation = () => {
             }
         };
 
-        try {
-            const res = await submit(router.query.otp);
-            const result = res.data?.result;
-            // 不需要做生日驗證
-            // localStorage.setItem('INCB', false);
-            console.log('=================', result);
-            if (res.data.success) {
-                const tokenVal = jwt_decode(getToken());
-                const checkData = checkCert(tokenVal.user_id);
-                console.log(tokenVal, checkData);
-                if (checkData.suggestAction != 'None') {
-                    // 有憑證送驗章
-                    const cert = await signCert({ idno: tokenVal.user_id }, true, token);
-                    const validateRes = await caValidator(getToken(), {
-                        signature: cert.signature,
-                        plainText: cert.plainText,
-                        certSN: cert.certSN,
-                        type: 'web',
-                    });
-                    console.log(validateRes);
-                    if (validateRes.msg !== '驗章成功') {
-                        console.log('清除憑證');
-                        if (validateRes.msg.split('||')[0].split('=')[1] === '8020') {
-                            clearCert();
-                            console.log('清除成功');
-                        }
+        // try {
+        const res = await submit(router.query.otp);
+        const result = res.data?.result;
+        // 不需要做生日驗證
+        // localStorage.setItem('INCB', false);
+        console.log('=================', result);
+        if (res.data.success) {
+            console.log(111111111111111111111111111111111111111111111111111111);
+            const tokenVal = jwt_decode(getToken());
+            console.log(tokenVal);
+            const checkData = checkCert(tokenVal.user_id);
+            console.log(checkData);
+            if (checkData.suggestAction == 'None') {
+                // 有憑證送驗章
+                const cert = await signCert({ idno: tokenVal.user_id }, true, tokenVal);
+                console.log('cert', cert);
+                const validateRes = await caValidator(getToken(), {
+                    signature: cert.signature,
+                    plainText: cert.plainText,
+                    certSN: cert.certSN,
+                    type: 'web',
+                });
+                console.log(validateRes);
+                if (validateRes.msg !== '驗章成功') {
+                    console.log('清除憑證');
+                    if (validateRes.msg.split('||')[0].split('=')[1] === '8020') {
+                        clearCert();
+                        console.log('清除成功');
                     }
                 }
-
-                if (router.query.platform) {
-                    // 新站一律使用 newweb_platform 作為 key，不與舊站 `platform` 與 `source` 兩個 key 混用，以保證舊站未來可安全下架
-                    setPlatform(router.query.platform.toLowerCase());
-                    // 為保持與舊站的相容，trust login 進站時，也要設定 sessionStorage `platform`，直到舊站下架才移除下面這行
-                    sessionStorage.setItem('platform', router.query.platform);
-                }
-
-                // http:// || https:// 開頭 全部直接幫忙轉到外部網址
-                if (/(^http(s?)):\/\//i.test(router.query.page)) {
-                    location.href = router.query.page;
-                } else {
-                    router.push(`/${router.query.page || ''}${getQueryStr()}`);
-                }
-            } else {
-                if (result.isOpenAccount) {
-                    Modal.confirm({
-                        content: '您尚未開立證券帳戶，請點選「確定」，即可進行線上開戶。',
-                        onOk: () => {
-                            window.open(result.url);
-                        },
-                        visible: true,
-                        okText: '確定',
-                        cancelText: '取消',
-                        onCancel: () => {
-                            location.href = `${process.env.NEXT_PUBLIC_SUBPATH}`;
-                        },
-                    });
-                }
             }
-        } catch (e) {
-            router.push(`/errPage`);
-            // 注意：這裡不需要再 alert 任何錯誤訊息，myAxios.js 已在上層處理 api 的 error handle (顯示錯誤訊息)
+
+            if (router.query.platform) {
+                // 新站一律使用 newweb_platform 作為 key，不與舊站 `platform` 與 `source` 兩個 key 混用，以保證舊站未來可安全下架
+                setPlatform(router.query.platform.toLowerCase());
+                // 為保持與舊站的相容，trust login 進站時，也要設定 sessionStorage `platform`，直到舊站下架才移除下面這行
+                sessionStorage.setItem('platform', router.query.platform);
+            }
+
+            // http:// || https:// 開頭 全部直接幫忙轉到外部網址
+            if (/(^http(s?)):\/\//i.test(router.query.page)) {
+                location.href = router.query.page;
+            } else {
+                router.push(`/${router.query.page || ''}${getQueryStr()}`);
+            }
+        } else {
+            if (result.isOpenAccount) {
+                Modal.confirm({
+                    content: '您尚未開立證券帳戶，請點選「確定」，即可進行線上開戶。',
+                    onOk: () => {
+                        window.open(result.url);
+                    },
+                    visible: true,
+                    okText: '確定',
+                    cancelText: '取消',
+                    onCancel: () => {
+                        location.href = `${process.env.NEXT_PUBLIC_SUBPATH}`;
+                    },
+                });
+            }
         }
+        // } catch (e) {
+        //     router.push(`/errPage`);
+        //     // 注意：這裡不需要再 alert 任何錯誤訊息，myAxios.js 已在上層處理 api 的 error handle (顯示錯誤訊息)
+        // }
     };
 
     useEffect(() => {
@@ -100,7 +103,11 @@ const Navigation = () => {
         }
     }, [router.query]);
 
-    return null;
+    return (
+        <>
+            <CaHead />
+        </>
+    );
 };
 
 Navigation.getLayout = Page => Page;
