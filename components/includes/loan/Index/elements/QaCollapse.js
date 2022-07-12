70fc356482c1322724dcccc441dd8d539da972e3
AnnLin <PUNKMOON0@gmail.com>
2022-07-12 15:22:32 +0800

    fix(借貸商品)改QABOX ID

--- components/includes/loan/Index/elements/QaCollapse.js
+++ components/includes/loan/Index/elements/QaCollapse.js
@@ -14,7 +14,7 @@ const QaCollapse = ({ dataSource }) => {
     };
 
     return (
-        <div className="qaBox" id="loanIndex">
+        <div className="qaBox" id="QaCollapse__content">
             <Collapse
                 onChange={onChange}
                 bordered={false}
@@ -75,26 +75,26 @@ const QaCollapse = ({ dataSource }) => {
             </style>
             <style jsx global>
                 {`
-                #loanIndex__container .qaBox .pageTag{position: absolute;top: -5em;}
-                #loanIndex__container .qaBox .ant-collapse-item {border:1px solid #d7e0ef;padding:10px 30px 10px;border-radius:2px;margin-bottom:15px;position: relative;}
-                #loanIndex__container .qaBox .ant-collapse-item-active {border-color:#daa360;}
-                #loanIndex__container .qaBox [data-theme='compact'] .site-collapse-custom-collapse .site-collapse-custom-panel,
-                #loanIndex__container .qaBox .site-collapse-custom-collapse .site-collapse-custom-panel {
+                #QaCollapse__content .pageTag{position: absolute;top: -5em;}
+                #QaCollapse__content .ant-collapse-item {border:1px solid #d7e0ef;padding:10px 30px 10px;border-radius:2px;margin-bottom:15px;position: relative;}
+                #QaCollapse__content .ant-collapse-item-active {border-color:#daa360;}
+                #QaCollapse__content [data-theme='compact'] .site-collapse-custom-collapse .site-collapse-custom-panel,
+                #QaCollapse__content .site-collapse-custom-collapse .site-collapse-custom-panel {
                     margin-bottom: 24px; overflow: hidden; background: #fff;border: 0px; border-radius: 2px;}
-                #loanIndex__container .qaBox .ant-collapse-header{ font-size: 20px;
+                #QaCollapse__content .ant-collapse-header{ font-size: 20px;
                     font-weight: 800; color:#0d1623;padding-left: 0px;padding-left: 0!important;}
-                #loanIndex__container .qaBox .ant-collapse-content-box{background:#f7ecde; font-size:16px; font-weight:500;margin-bottom:7px;}
-                #loanIndex__container .qaBox .ant-collapse-content-box p{margin:0;}
-                #loanIndex__container .plusIcon{ border-radius:2px; right: 0; left:auto!important; background:#a9b6cb; text-align:center; width: 26px;
+                #QaCollapse__content .ant-collapse-content-box{background:#f7ecde; font-size:16px; font-weight:500;margin-bottom:7px;}
+                #QaCollapse__content .ant-collapse-content-box p{margin:0;}
+                #QaCollapse__content .plusIcon{ border-radius:2px; right: 0; left:auto!important; background:#a9b6cb; text-align:center; width: 26px;
                     height: 26px; display: inline-block;}
-                #loanIndex__container .plusIcon i{display: block; color:#FFF;transform:rotate(0deg);font-size:24px;font-style: inherit;
+                #QaCollapse__content .plusIcon i{display: block; color:#FFF;transform:rotate(0deg);font-size:24px;font-style: inherit;
                     font-weight: 300;line-height:26px!important;}
-                #loanIndex__container .plusIcon.active{background: #daa360;!important;}
-                #loanIndex__container .plusIcon.active i{transform:rotate(45deg);}
+                #QaCollapse__content .plusIcon.active{background: #daa360;!important;}
+                #QaCollapse__content .plusIcon.active i{transform:rotate(45deg);}
                 @media screen and (max-width: 425px) {
-                    #loanIndex__container .qaBox .ant-collapse-item{padding:4%;}
-                    #loanIndex__container .qaBox .ant-collapse-header{ font-size: 16px;padding-right: 30px!important; line-height: 1.3!important;}
-                    #loanIndex__container .ant-collapse>.ant-collapse-item>.ant-collapse-header .ant-collapse-arrow {top: 16%; transform: translateY(0%);}
+                    #QaCollapse__content .ant-collapse-item{padding:4%;}
+                    #QaCollapse__content .ant-collapse-header{ font-size: 16px;padding-right: 30px!important; line-height: 1.3!important;}
+                    #QaCollapse__content .ant-collapse>.ant-collapse-item>.ant-collapse-header .ant-collapse-arrow {top: 16%; transform: translateY(0%);}
                 }
                 `}
             </style>
